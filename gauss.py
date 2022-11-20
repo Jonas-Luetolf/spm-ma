ff560da03d59a51fcc199bd58f15c104ec53d3c0
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-11-20 17:26:56 +0100

    gauss add adjust matrix

--- gauss.py
+++ gauss.py
@@ -1,4 +1,25 @@
 import numpy as np
+import sys
+
+def check_matrix(m:np.ndarray)->bool:
+    for x in range(m.shape[1]-1):
+        if m[x,x]==0:
+            return False
+    return True
+
+def adjust_matrix(m):
+    for x in range(m.shape[1]-1):
+        if m[x,x]==0:
+            for y,n in enumerate(m[x:,x]):
+                if n!=0:
+                    m[[x,y+x]]=m[[y+x,x]]
+                    break
+
+    if not check_matrix(m):
+        sys.exit(-1)
+        
+    return m
+
 
 def solve_gauss(m):
     for x in range(0,m.shape[1]-1):
@@ -12,8 +33,12 @@ def solve_gauss(m):
 
 
 if __name__ == "__main__":
-    matrix=np.array([[4,9,-8,5],[3,-6,16,5],[5,3,4,7]],dtype=np.float128)
-    results=solve_gauss(matrix)
+    matrix=np.array([[4,6,2,22],[3,0,2,9],[5,1,1,10]],dtype=np.float128)
+    if not check_matrix(matrix):
+        matrix=adjust_matrix(matrix)
 
-    for i,result in enumerate(results):
-        print(f"{i+1}. Variable: {result}")
+
+        results=solve_gauss(matrix)
+
+        for i,result in enumerate(results):
+            print(f"{i+1}. Variable: {result}")
