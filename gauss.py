28f2ac1c936be32980c2a3a028eee3c566ea8d4d
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-11-20 19:58:16 +0100

    update gauss adjust_matrix

--- gauss.py
+++ gauss.py
@@ -1,27 +1,39 @@
 import numpy as np
 import sys
 
-def check_matrix(m:np.ndarray)->bool:
-    for x in range(m.shape[1]-1):
-        if m[x,x]==0:
-            return False
-    return True
+class notsolvable(Exception):
+    def __init__(self):
+        super().__init__()
+    def __str__(self) -> str:
+        return "not solvable"
 
-def adjust_matrix(m):
+def check_matrix(m:np.ndarray)->int:
+    num_zeros:int=0
     for x in range(m.shape[1]-1):
         if m[x,x]==0:
-            for y,n in enumerate(m[x:,x]):
+            num_zeros+=1
+    return num_zeros
+
+def adjust_matrix(m:np.ndarray)->np.ndarray:
+    num_zeros:int=check_matrix(m)
+    for x in range(m.shape[1]-1):
+        if m[x,x]==0:
+            for y,n in enumerate(m[:,x]):
+                temp_m:np.ndarray=m
                 if n!=0:
-                    m[[x,y+x]]=m[[y+x,x]]
-                    break
-
-    if not check_matrix(m):
-        sys.exit(-1)
-        
+                    temp_m[[x,y]]=temp_m[[y,x]]
+                    if check_matrix(temp_m)<num_zeros:
+                       m[[x,y+x]]=m[[y+x,x]]
+                       num_zeros=check_matrix(m)
+                       break
+                    else:
+                        continue
+    if check_matrix(m)!=0:
+        raise notsolvable()
     return m
 
 
-def solve_gauss(m):
+def solve_gauss(m:np.ndarray)->list:
     for x in range(0,m.shape[1]-1):
         m[x,:]=m[x,:]/m[x,x]
         
@@ -33,12 +45,12 @@ def solve_gauss(m):
 
 
 if __name__ == "__main__":
-    matrix=np.array([[4,6,2,22],[3,0,2,9],[5,1,1,10]],dtype=np.float128)
-    if not check_matrix(matrix):
+    matrix:np.ndarray=np.array([[0,1,0,2],[0,0,1,3],[1,0,0,1]],dtype=np.float128)
+    if check_matrix(matrix)>0:
         matrix=adjust_matrix(matrix)
 
-
-        results=solve_gauss(matrix)
+        print(matrix)
+        results:list=solve_gauss(matrix)
 
         for i,result in enumerate(results):
             print(f"{i+1}. Variable: {result}")
