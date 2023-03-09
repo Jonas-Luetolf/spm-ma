2dbf0c034dc76cdf1d6c0300af4e2fd5d62ba647
Jonas Lütolf <jonas.luetolf@outlook.com>
2023-03-09 21:19:47 +0100

    black code style

--- numpy-test.py
+++ numpy-test.py
@@ -10,12 +10,12 @@ matrix = np.array([[1, 2, 3], [4, 5, 6]])
 
 
 start1 = time.time_ns()
-for i in range(10**6):
+for i in range(10 ** 6):
     np.linalg.det(matrix)
 end1 = time.time_ns()
 
 start2 = time.time_ns()
-for i in range(10**6):
+for i in range(10 ** 6):
     det(matrix)
 end2 = time.time_ns()
 
